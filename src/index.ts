b133169ab818094b3e2ed0761dbdaaf6eb1ef5cf
Marcelo Moura de Lima <marcelogoodtrip@gmail.com>
2022-08-05 19:53:41 -0400

    bearer autentication

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import bearerAuthenticationMiddleware from "./middlewares/bearer-authentication.middleware";
 import errorHandler from "./middlewares/error-handler.middleware";
 import authorizationRoute from "./routes/authorization.route";
 import statusRoute from "./routes/status.route";
@@ -12,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 
 //  Configurações de Rotas
 app.use(usersRoute);
-app.use(statusRoute);
+app.use(bearerAuthenticationMiddleware, statusRoute);
 app.use(authorizationRoute);
 
 //  Configuração dos Handlers de Erro
