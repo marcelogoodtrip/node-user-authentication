d201091153789db3395acd5ef0a50703b951c694
Marcelo Moura de Lima <marcelogoodtrip@gmail.com>
2022-08-06 02:40:42 -0400

    final alters

--- src/db.ts
+++ src/db.ts
@@ -1,6 +1,6 @@
 import { Pool } from "pg";
 
-const connectionString = `postgres://bijvepbj:sE75Yc5fs2m8nWpfoUn8SMqMmDh8bS7I@kesavan.db.elephantsql.com/bijvepbj`;
+const connectionString = ``;
 
 const db = new Pool({ connectionString });
 
--- src/routes/authorization.route.ts
+++ src/routes/authorization.route.ts
@@ -1,7 +1,8 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { StatusCodes } from "http-status-codes";
-import JWT from 'jsonwebtoken';
+import JWT, { SignOptions } from 'jsonwebtoken';
 import basicAuthenticationMiddleware from "../middlewares/basic-authentication.middleware";
+import jwtAuthenticationMiddleware from "../middlewares/jwt-authentication.middleware";
 import ForbiddenError from "../models/forbidden.error.model";
 
 const authorizationRoute = Router();
@@ -11,12 +12,12 @@ authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Req
         const user = req.user;
 
         if(!user) {
-            throw new ForbiddenError('Usuário não encontrado');
+            throw new ForbiddenError('Usuário não informado');
         }
 
         const jwtPayload = { username: user.username };
-        const jwtOptions = { subject: user?.uuid };
-        const secretKey = 'mt_secret_key'
+        const jwtOptions: SignOptions = { subject: user?.uuid, expiresIn: '15m' };
+        const secretKey = 'my_secret_key';
 
         const jwt = JWT.sign(jwtPayload, secretKey, jwtOptions);
         res.status(StatusCodes.OK).json({ token: jwt });
@@ -24,6 +25,10 @@ authorizationRoute.post('/token', basicAuthenticationMiddleware, async (req: Req
     } catch (error) {
         next(error);
     }
+});
+
+authorizationRoute.post('/token/validate', jwtAuthenticationMiddleware, (req: Request, res: Response, next: NextFunction) => {
+    res.sendStatus(StatusCodes.OK);
 });
 
 export default authorizationRoute;
